980ae65602d6e2d3296f9134a274d4867b7ac1a5
Krediann <Roope98.rl@gmail.com>
2022-04-22 02:52:53 -0700

    Created the model for user

---
+++ models/user.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const mongoose = require("mongoose"),
+    {Schema} = mongoose
+
+    userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
