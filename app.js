2baf6ecd057ccea7b1a8d16b7fc715f117555083
Krediann <Roope98.rl@gmail.com>
2022-04-22 02:42:13 -0700

    Added connection to mdatabase using mongoose

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const mongoose = require("mongoose");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//Setting mongoose connection
+mongoose.connect(
+  "mongodb://localhost:27017/groupwork_db",
+  { useNewUrlParser: true}
+);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Successfully connected to MongoDB using Mongoose!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
